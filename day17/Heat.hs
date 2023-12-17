392d362a1d2c9bed1580e95eab09be1f338b869f
davidaf3 <david.alvarez.fidalgo@gmail.com>
2023-12-17 23:38:33 +0100

    Forgot to use h

--- day17/Heat.hs
+++ day17/Heat.hs
@@ -17,13 +17,13 @@ type State = (Point, (Int, Int), Int)
 
 type NextSpds = Int -> (Int, Int) -> [(Int, Int)]
 
-data WithCost a = WithCost Int a
+data WithCost a = WithCost Int Int a
 
 instance Eq (WithCost a) where
-  (==) (WithCost c1 _) (WithCost c2 _) = c1 == c2
+  (==) (WithCost c1 _ _) (WithCost c2 _ _) = c1 == c2
 
 instance Ord (WithCost a) where
-  compare (WithCost c1 _) (WithCost c2 _) = compare c1 c2
+  compare (WithCost c1 _ _) (WithCost c2 _ _) = compare c1 c2
 
 parseGrid :: [String] -> Grid
 parseGrid = V.fromList . map (V.fromList . map (read . singleton))
@@ -38,13 +38,13 @@ isFinalCrucible :: Point -> State -> Bool
 isFinalCrucible goal (p, _, _) = goal == p
 
 expandCrucible :: NextSpds -> Grid -> WithCost State -> [WithCost State]
-expandCrucible nextSpds grid (WithCost cost ((x, y), spd, sameDir)) =
+expandCrucible nextSpds grid (WithCost hCost gCost ((x, y), spd, sameDir)) =
   mapMaybe next $ nextSpds sameDir spd
   where
     next spd'@(spdX', spdY') =
       let p' = (x + spdX', y + spdY')
           sameDir' = if spd' == spd then sameDir + 1 else 0
-       in getHeat grid p' <&> (\heat -> WithCost (cost + heat) (p', spd', sameDir'))
+       in getHeat grid p' <&> (\heat -> WithCost hCost (gCost + heat) (p', spd', sameDir'))
 
 getNextSpds :: NextSpds
 getNextSpds sameDir spd@(spdX, spdY) =
@@ -65,8 +65,9 @@ astar isFinal h expand init =
   let initQueue = foldr PQ.insert PQ.empty init
    in go initQueue Set.empty
   where
+    f (WithCost _ cost state) = WithCost (cost + h state) cost state
     go queue visited =
-      let (wc@(WithCost _ state), queue') = PQ.deleteFindMin queue
+      let (wc@(WithCost _ _ state), queue') = PQ.deleteFindMin queue
        in if Set.member state visited
             then go queue' visited
             else
@@ -74,15 +75,15 @@ astar isFinal h expand init =
                 then wc
                 else
                   let visited' = Set.insert state visited
-                      queue'' = foldr PQ.insert queue' $ expand wc
+                      queue'' = foldr (PQ.insert . f) queue' (expand wc)
                    in go queue'' visited'
 
 main :: IO ()
 main = do
   grid <- readFile "input.txt" <&> parseGrid . lines
   let initStates =
-        [ WithCost 0 ((0, 0), (1, 0), 0),
-          WithCost 0 ((0, 0), (0, 1), 0)
+        [ WithCost (h ((0, 0), (1, 0), 0)) 0 ((0, 0), (1, 0), 0),
+          WithCost (h ((0, 0), (0, 1), 0)) 0 ((0, 0), (0, 1), 0)
         ]
       rows = V.length grid
       cols = V.length (grid ! 0)
@@ -91,7 +92,7 @@ main = do
       h = manhattanDist goal
       expand = expandCrucible getNextSpds grid
       expandUltra = expandCrucible getNextSpdsUltra grid
-      (WithCost cost _) = astar isFinal h expand initStates
-      (WithCost cost' _) = astar isFinal h expandUltra initStates
+      (WithCost _ cost _) = astar isFinal h expand initStates
+      (WithCost _ cost' _) = astar isFinal h expandUltra initStates
   print cost
   print cost'
