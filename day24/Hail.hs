3ff84bf73b69e7b6dadb19cb26173bfa907ff314
davidaf3 <david.alvarez.fidalgo@gmail.com>
2023-12-26 20:21:43 +0100

    Use theomem prover

--- day24/Hail.hs
+++ day24/Hail.hs
@@ -2,20 +2,19 @@
 
 module Main where
 
-import Data.Functor (($>), (<&>))
-import Data.List (intercalate)
+import Data.Functor ((<&>))
+import Data.Maybe (fromJust)
+import Data.SBV (Symbolic, constrain, getModelValue, sInteger, sat, (.==), (.>=))
 import Data.Void (Void)
-import Debug.Trace (trace)
-import GaussElimination (solve)
 import Text.Megaparsec (Parsec, errorBundlePretty, parse, sepBy1, some)
-import Text.Megaparsec.Char (char, eol, spaceChar, string)
-import Text.Megaparsec.Char.Lexer (decimal, signed, space)
+import Text.Megaparsec.Char (char, eol, spaceChar)
+import Text.Megaparsec.Char.Lexer (decimal, signed)
 
-type Point = (Rational, Rational, Rational)
+type Point a = (a, a, a)
 
-type Line = (Point, Point)
+type Line a = (Point a, Point a)
 
-parseLine :: Parsec Void String Line
+parseLine :: Parsec Void String (Line Int)
 parseLine = do
   [x, y, z] <- number `sepBy1` (char ',' >> some spaceChar)
   some spaceChar >> char '@' >> some spaceChar
@@ -24,14 +23,20 @@ parseLine = do
   where
     number = signed (return ()) decimal
 
-intersect :: Line -> Line -> Maybe (Rational, Rational)
+toRat :: Line Int -> Line Rational
+toRat ((x, y, z), (vx, vy, vz)) =
+  ( (fromIntegral x, fromIntegral y, fromIntegral z),
+    (fromIntegral vx, fromIntegral vy, fromIntegral vz)
+  )
+
+intersect :: Line Rational -> Line Rational -> Maybe (Rational, Rational)
 intersect ((x1, y1, _), (vx1, vy1, _)) ((x2, y2, _), (vx2, vy2, _)) =
   let divisor = vy1 / vx1 - vy2 / vx2
       x = (y2 - (vy2 / vx2) * x2 - y1 + (vy1 / vx1) * x1) / divisor
       y = y1 + vy1 * ((x - x1) / vx1)
    in if divisor /= 0 then Just (x, y) else Nothing
 
-intersectInside :: Rational -> Rational -> (Line, Line) -> Bool
+intersectInside :: Rational -> Rational -> (Line Rational, Line Rational) -> Bool
 intersectInside
   minCoord
   maxCoord
@@ -52,26 +57,21 @@ pairs :: [a] -> [(a, a)]
 pairs [_] = []
 pairs (x : xs) = map (x,) xs ++ pairs xs
 
-extractXY :: Line -> (Rational, Rational, Rational, Rational)
-extractXY ((x, y, _), (vx, vy, _)) = (x, y, vx, vy)
+findIntersecting :: [Line Int] -> Symbolic ()
+findIntersecting lines = do
+  [x, y, z, vx, vy, vz] <- mapM sInteger ["x", "y", "z", "vx", "vy", "vz"]
+  let constrainLine (i, ((xi, yi, zi), (vxi, vyi, vzi))) = do
+        ti <- sInteger ("t" ++ show i)
+        constrain $ ti .>= 0
+        constrain $ x + vx * ti .== fromIntegral xi + fromIntegral vxi * ti
+        constrain $ y + vy * ti .== fromIntegral yi + fromIntegral vyi * ti
+        constrain $ z + vz * ti .== fromIntegral zi + fromIntegral vzi * ti
+  foldMapM constrainLine $ zip [1 ..] $ take 3 lines
 
-extractXZ :: Line -> (Rational, Rational, Rational, Rational)
-extractXZ ((x, _, z), (vx, _, vz)) = (x, z, vx, vz)
-
-toEqSystem :: [Line] -> (Line -> (Rational, Rational, Rational, Rational)) -> [[Rational]]
-toEqSystem lines extract = go $ take 4 $ pairs $ map extract lines
-  where
-    go [] = []
-    go
-      (((a1, b1, va1, vb1), (a2, b2, va2, vb2)) : ls) =
-        let eq =
-              [ vb2 - vb1,
-                va1 - va2,
-                b1 - b2,
-                a2 - a1,
-                a2 * vb2 - b2 * va2 - a1 * vb1 + b1 * va1
-              ]
-         in eq : go ls
+foldMapM :: (Monad m) => (a -> m b) -> [a] -> m b
+foldMapM _ [] = error "foldMapM: empty list"
+foldMapM f [x] = f x
+foldMapM f (x : xs) = f x >> foldMapM f xs
 
 doParse :: Parsec Void String a -> String -> a
 doParse parser = either (error . errorBundlePretty) id . parse parser ""
@@ -79,7 +79,7 @@ doParse parser = either (error . errorBundlePretty) id . parse parser ""
 main :: IO ()
 main = do
   lines <- readFile "input.txt" <&> doParse (parseLine `sepBy1` eol)
-  print $ length $ filter (intersectInside 200000000000000 400000000000000) $ pairs lines
-  let [x, y, _, _] = solve $ toEqSystem lines extractXY
-      [_, z, _, _] = solve $ toEqSystem lines extractXZ
-  print $ round $ x + y + z
+  let ratPairs = pairs $ map toRat lines
+  print $ length $ filter (intersectInside 200000000000000 400000000000000) ratPairs
+  model <- sat (findIntersecting lines)
+  print $ sum (map (fromJust . flip getModelValue model) ["x", "y", "z"] :: [Integer])
